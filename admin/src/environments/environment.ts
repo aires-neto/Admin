6b0a00c5ed8c5f0cf39f989cd39cdf5ed298026a
aires-neto <aires_neto@hotmail.com>
2020-08-24 12:27:36 -0300

    comit2

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyCED3LJCajz-cX-xzWDbZh6rPRl7RRFnHE",
+    authDomain: "admin-16698.firebaseapp.com",
+    databaseURL: "https://admin-16698.firebaseio.com",
+    projectId: "admin-16698",
+    storageBucket: "admin-16698.appspot.com",
+    messagingSenderId: "1076864246957",
+    appId: "1:1076864246957:web:8746cba96faf82867ad224",
+    measurementId: "G-52EC6FLR3B"
+  }
 };
 
 /*
